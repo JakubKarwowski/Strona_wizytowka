a2d2a9c89effb2d265505a4e110b9232a1e7b0af
JakubKarwowski <jakubkarwowski95@gmail.com>
2021-03-02 17:49:25 +0100

    misiu

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Początek !!!</h1>
+        <h1><i class="fas fa-heart"></i>Kocham Cię Misiaczku!!!<i class="fas fa-heart"></i>
+        </h1>
       </header>
     </div>
   );
