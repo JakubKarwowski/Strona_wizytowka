f1fc583da6d76d2f895cea7ba8a4e48821663634
JakubKarwowski <jakubkarwowski95@gmail.com>
2021-03-03 20:44:34 +0100

    repairing import

--- src/Body.js
+++ src/Body.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Body.scss'
-import {BrowserRouter as Router, Route, Switch,} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 
 const start = ()=>{
     return(
