1778c8e149c83e784d1217a5f0d6111e5fbb49d1
JakubKarwowski <jakubkarwowski95@gmail.com>
2021-03-09 11:32:24 +0100

    adding about section

--- src/body_components/About.js
+++ src/body_components/About.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import '../body_styles/About.scss'
+import data_about from "./body_data/data_about.json"
+import About_Create from "./About_create"
+
+
+
 
 class About extends React.Component{
-    render() {
+
+    render() {  
+        
         return (  
-            <div className="about">
-                <p>About</p>
+            <div className="about">   
+                <h1>Want to know my hobbies? :)</h1>          
+                <About_Create items= {data_about.items}/>
             </div>
 
         );
     };
-}
+   
+    
+};
 
 export default About;
---
+++ src/body_components/About_create.js
@@ -0,0 +1,33 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+function About_Create (props) {
+
+    const createElements = props.items.map
+    (item=>{
+        return(
+            <div key={item.icon} className={item.classname}>
+                <span className= 'icon'>
+                    <FontAwesomeIcon icon={item.icon}/>
+                </span>
+                <section className='iconDescription'>
+                    <h2>{item.title}</h2>
+                    <p>{item.description}</p>
+                </section>
+            </div>
+        )
+    });
+    // console.log(createElements)
+    return createElements;
+            
+        
+    
+}
+
+export default About_Create;
+
+
+
+
+{/*<button className='music' ></button> */}
