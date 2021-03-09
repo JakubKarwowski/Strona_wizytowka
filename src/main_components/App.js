afbc0390a165e382e4693187274a442d930ccac5
JakubKarwowski <jakubkarwowski95@gmail.com>
2021-03-09 11:32:11 +0100

    importing font awesome globally

--- src/main_components/App.js
+++ src/main_components/App.js
@@ -3,6 +3,10 @@ import Header from './Header'
 import Body from './Body'
 import Footer from './Footer'
 import {BrowserRouter as Router} from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMusic, faBasketballBall, faCar, faMoneyBillWave,faClock, faBicycle, faDumbbell, faBookReader } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMusic, faBasketballBall, faCar, faMoneyBillWave,faClock, faBicycle, faDumbbell, faBookReader)
 
 function App() {
   return (
